9627899b3651b2fac13333e526887cb0651eb559
Mehwish <mehwishgul715@gmail.com>
2023-06-19 21:42:27 +0500

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+
+
+// ===================================dataTables
+let table = new DataTable('#myTable', {
+    // "bPaginate": false,
+    // "bLengthChange": false,
+    language: { search: "",searchPlaceholder: "Search..." },
+    // "bFilter": false,
+    // "bInfo": false,
+//  "bAutoWidth": false
+  });
