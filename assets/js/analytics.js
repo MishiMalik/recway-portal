3a7ecac7e529a9e4701a28778385b35337767fa9
Mehwish <mehwishgul715@gmail.com>
2023-06-24 18:25:58 +0500

    commit

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -1,36 +1,169 @@
+  
+//    Charrts 1
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
 
-
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawVisualization);
-
-function drawVisualization() {
-  // Some raw data (not necessarily accurate)
-  var data = google.visualization.arrayToDataTable([
-    ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-    ['2004/05',  165,      938,         522,             998,           450,      614.6],
-    ['2005/06',  135,      1120,        599,             1268,          288,      682],
-    ['2006/07',  157,      1167,        587,             807,           397,      623],
-    ['2007/08',  139,      1110,        615,             968,           215,      609.4],
-    ['2008/09',  136,      691,         629,             1026,          366,      569.6]
-  ]);
-
-  var options = {
-    // title : 'Monthly Coffee Production by Country',
-    
-    seriesType: 'bars',
-    series: {5: {type: 'line'}}
-  };
-
-  var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
-  var chart2 = new google.visualization.ComboChart(document.getElementById('chart_div2'));
-  var chart3 = new google.visualization.ComboChart(document.getElementById('chart_div3'));
-  var chart4 = new google.visualization.ComboChart(document.getElementById('chart_div4'));
-  chart.draw(data, options);
-  chart2.draw(data, options);
-  chart3.draw(data, options);
-  chart4.draw(data, options);
-  var chart5 = new google.visualization.ComboChart(document.getElementById('chart_div5'));
-  chart5.draw(data, options);
-  var chart6 = new google.visualization.ComboChart(document.getElementById('chart_div6'));
-  chart6.draw(data, options);
+new Chart("myChart", {
+type: "horizontalBar",
+data: {
+  labels: xValues,
+  datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "#7367f0",
+      borderColor: "#7367f0",
+      data: yValues
+  }]
+},
+options: {
+  legend: { display: false },
+  scales: {
+      yAxes: [{
+          ticks: { min: 6, max: 16 },
+          indexAxis: 'y'
+      }],
+  }
 }
+});
+
+//   chart 3
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("myChart3", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "#7367f0",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        scales: {
+            yAxes: [{
+                ticks: { min: 6, max: 16 },
+                indexAxis: 'y'
+            }],
+        }
+    }
+});
+
+
+//   chart 2
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("myChart2", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "#7367f0",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        scales: {
+            yAxes: [{
+                ticks: { min: 6, max: 16 },
+                indexAxis: 'y'
+            }],
+        }
+    }
+});
+
+//   chart 4
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("myChart4", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "#7367f0",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        scales: {
+            yAxes: [{
+                ticks: { min: 6, max: 16 },
+                indexAxis: 'y'
+            }],
+        }
+    }
+});
+
+
+//   chart 5
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("myChart5", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "#7367f0",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        scales: {
+            yAxes: [{
+                ticks: { min: 6, max: 16 },
+                indexAxis: 'y'
+            }],
+        }
+    }
+});
+
+
+//   chart 6
+var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+new Chart("myChart6", {
+    type: "line",
+    data: {
+        labels: xValues,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "#7367f0",
+            borderColor: "rgba(0,0,255,0.1)",
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        scales: {
+            yAxes: [{
+                ticks: { min: 6, max: 16 },
+                indexAxis: 'y'
+            }],
+        }
+    }
+});
+
+
+
+
